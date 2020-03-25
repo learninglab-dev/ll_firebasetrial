66953b1fe59039fc7f0cfc53b9448a83953122df
sophiebaud11 <38045114+sophiebaud11@users.noreply.github.com>
2020-03-25 11:28:27 -0400

    firstcommit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyAnS56HAtI1frAuZPhcXFyIBWFkZI7Osjo",
+    authDomain: "ll-tutorial.firebaseapp.com",
+    databaseURL: "https://ll-tutorial.firebaseio.com",
+    projectId: "ll-tutorial",
+    storageBucket: "ll-tutorial.appspot.com",
+    messagingSenderId: "304967336251",
+    appId: "1:304967336251:web:561dc775a70d926513b71e"
+  };
+
+export default config;
